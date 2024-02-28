57fa8d6c14856655d43da0a2a5fd5eb81c188e66
John Contreras <john.contreras.medina@gmail.com>
2024-02-27 22:22:01 -0500

    adding index, and first js

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const cajaEncriptar = document.querySelector('#texto-encriptar');
+const cajaDesencriptar = document.querySelector('texto-desencriptar');
+
+
+
+function texto1(){
+    console.log(cajaEncriptar.value);
+
+}
